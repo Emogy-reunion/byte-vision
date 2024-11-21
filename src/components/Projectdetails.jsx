c93530b1d75b3c06dbce48acc37023f3148df70f
Emogy-reunion <mv7786986@gmail.com>
2024-11-21 10:37:19 +0300

    import images correctly

--- src/components/Projectdetails.jsx
+++ src/components/Projectdetails.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '/src/assets/css/projects.css';
+import emImage from '/src/assets/images/em.png';
+import soonImage from '/src/assets/images/soon.jpeg';
 
 const ProjectDetails = () => {
 
@@ -9,25 +11,25 @@ const ProjectDetails = () => {
 			<div className='project-container'>
 				<a href='#'>
 					<div className='project'>
-						<img src='/src/assets/images/em.png' alt='project' />
+						<img src={emImage} alt='project' />
 					</div>
 				</a>
 
 				<a href='#'>
 					<div className='project'>
-                        		        <img src='/src/assets/images/soon.jpeg' alt='project' />
+                        		        <img src={soonImage} alt='project' />
                        			</div>   
 				</a>
 
 				<a href='#'>
 					<div className='project'>
-                	                	<img src='/src/assets/images/soon.jpeg' alt='project' />
+                	                	<img src={soonImage} alt='project' />
                         		</div>
 				</a>
 
 				<a href='#'>
 					<div className='project'>
-                                		<img src='/src/assets/images/soon.jpeg' alt='project' />
+                                		<img src={soonImage} alt='project' />
                        			</div>
 				</a>
 			</div>

58ff60466be0c496dce6caeea5da7e843c348e55
Emogy-reunion <mv7786986@gmail.com>
2024-11-21 10:23:03 +0300

    fix: import images

--- src/components/Homesample.jsx
+++ src/components/Homesample.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import '/src/assets/css/homesample.css';
+import sample1 from '/src/assets/images/sample1.jpeg';
+import sample2 from '/src/assets/images/sample2.jpeg';
+import sample3 from '/src/assets/images/sample3.jpeg';
+import sample4 from '/src/assets/images/sample4.jpeg';
+import em from '/src/assets/images/em.png';
+import web from '/src/assets/images/web.jpeg';
+import sample7 from '/src/assets/images/sample7.jpeg';
 
 const HomeSample = () => {
 	return (
@@ -7,34 +14,34 @@ const HomeSample = () => {
 
 			<div className='sample-container'>
 				<div className='samples'>
-					<img src='/src/assets/images/sample1.jpeg' alt='sample' />
+					<img src={sample1} alt='sample' />
 				</div>
 
 				<div className='samples'>
-                                        <img src='/src/assets/images/sample2.jpeg' alt='sample' />
+                                        <img src={sample2} alt='sample' />
                                 </div>
 
 				<div className='samples'>
-                                        <img src='/src/assets/images/sample3.jpeg' alt='sample' />
+                                        <img src={sample3} alt='sample' />
                                 </div>
 
 				<div className='samples'>
-                                        <img src='/src/assets/images/sample4.jpeg' alt='sample' />
+                                        <img src={sample4} alt='sample' />
                                 </div>
 			</div>
 
 			<div className='sample-container'>
 				<div className='web'>
-					<img src='/src/assets/images/em.png' alt='sample' />
+					<img src={em} alt='sample' />
 				</div>
 
 				<div className='web'>
-					<img src='/src/assets/images/web.jpeg' alt='sample' />
+					<img src={web} alt='sample' />
 
 				</div>
 
 				<div className='samples hide-sample'>
-                                        <img src='/src/assets/images/sample7.jpeg' alt='sample' />
+                                        <img src={sample7} alt='sample' />
 
                                 </div>
 			</div>

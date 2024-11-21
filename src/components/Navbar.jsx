b8f3ca443d64bd3478951b4948eab88294de104c
Emogy-reunion <mv7786986@gmail.com>
2024-11-21 16:46:19 +0300

    add active nav link styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '/src/assets/css/navbar.css';
 
 
@@ -19,21 +19,21 @@ const NavBar = () => {
         return (
                 <nav>
                         <ul>
-                                <li className='hide-on-mobile'><Link to='/' className='nav-link'>Home</Link></li>
-                                <li className='hide-on-mobile'><Link to='/about' className='nav-link'>About Us</Link></li>
-                                <li className='hide-on-mobile'><Link to='/services' className='nav-link'>Services</Link></li>
-                                <li className='hide-on-mobile'><Link to='/projects' className='nav-link'>Projects</Link></li>
-                                <li className='hide-on-mobile'><Link to='/gallery' className='nav-link'>Gallery</Link></li>
-                                <li className="menu-button"><Link to="#" className='nav-link' onClick={ShowSidebar}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></Link></li>
+                                <li className='hide-on-mobile'><NavLink to='/' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Home</NavLink></li>
+                                <li className='hide-on-mobile'><NavLink to='/about' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>About Us</NavLink></li>
+                                <li className='hide-on-mobile'><NavLink to='/services' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Services</NavLink></li>
+                                <li className='hide-on-mobile'><NavLink to='/projects' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Projects</NavLink></li>
+                                <li className='hide-on-mobile'><NavLink to='/gallery' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Gallery</NavLink></li>
+                                <li className="menu-button"><NavLink to="#" className='nav-link' onClick={ShowSidebar}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></NavLink></li>
                         </ul>
 
                         <ul className={ isHidden ? 'hide-sidebar' : 'sidebar'}>
-                                <li><Link to="#" className='nav-link' onClick={HideSidebar}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></Link></li>
-                                <li><Link to='/' className='nav-link'>Home</Link></li>
-                                <li><Link to='/about' className='nav-link'>About Us</Link></li>
-                                <li><Link to='/services' className='nav-link'>Services</Link></li>
-                                <li><Link to='/projects' className='nav-link'>Projects</Link></li>
-                                <li><Link to='/gallery' className='nav-link'>Gallery</Link></li>
+                                <li><NavLink to="#" className='nav-link' onClick={HideSidebar}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></NavLink></li>
+                                <li><NavLink to='/' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Home</NavLink></li>
+                                <li><NavLink to='/about' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>About Us</NavLink></li>
+                                <li><NavLink to='/services' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Services</NavLink></li>
+                                <li><NavLink to='/projects' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Projects</NavLink></li>
+                                <li><NavLink to='/gallery' className={({ isActive }) => isActive ? 'nav-link active-link': 'nav-link'}>Gallery</NavLink></li>
                         </ul>
                 </nav>
         );
